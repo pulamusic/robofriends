211e2ce05f8b480d96d13b2d69c3585c542037d8
jim@pulamusic.com <jim@pulamusic.com>
2020-12-12 15:29:08 -0500

    new build

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Scroll = (props) => {
   // console.log(props)
   return (
-    <div style={{ overflowY: 'scroll', border: '5px solid black', height: '800px' }}>
+    <div style={{ overflowY: 'scroll', border: '5px solid black', height: '850px' }}>
       {props.children}
     </div>
   )
